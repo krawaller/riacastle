66f1f687e7ddad7d73f888cc2539b1360e1c012d
KungD <david@krawaller.se>
2013-11-08 09:49:11 +0100

    graph fix

--- src/views/graphicsoverview.js
+++ src/views/graphicsoverview.js
@@ -1,8 +1,9 @@
 define(["backbone","jquery","underscore","json!data/static/graphics.json","data/data"],function(Backbone,$,_,graphics,data){
 	return Backbone.View.extend({
 		render: function(){
-			this.$el.html(_.reduce(_.ensureArray(this.options.kinds,_.keys(graphics)),function(html,kind){
-				return html+"<h2>"+kind+"</h2><p>"+_.reduce(graphics[kind].urls,function(str,url){
+			var kinds = this.options.kinds;
+			this.$el.html(_.reduce(_.ensureArray(kinds,_.keys(graphics)),function(html,kind){
+				return html+(kinds.length>1?"<h2>"+kind+"</h2>":"")+"<p>"+_.reduce(graphics[kind].urls,function(str,url){
 					return str+"<img "+(_.indexOf(data.icons,url)>-1?"class='chosen' ":"")+"title='"+url+"' alt='"+url+"' src='"+graphics[kind].pre+url+".png'/>";
 				},"",this)+"</p>";
 			},"",this));
